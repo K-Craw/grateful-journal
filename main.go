95236b3a870efd8e3e6d195e86c80bd7c8961513
Ahmed El-Gaaly <elgaalya@gmail.com>
2022-05-20 18:19:06 -0400

    added array of possible messages, main, getentry, and started create entry

--- main.go
+++ main.go
@@ -12,3 +12,27 @@ type entry struct {
 	Date    string json: "date"
 	User_ID string json: "user_id"
 }
+
+var entries = []webpost{
+	{ID: "1", Entry: "My first message", Date: "05/18/2022", User_ID: "user1"},
+	{ID: "2", Entry: "Hi name is user2", Date: "05/19/2022", User_ID: "user2"},
+	{ID: "3", Entry: "I made third message", Date: "05/20/2022", User_ID: "user1"}
+}
+
+func createEntry(c *gin.Context) {
+	var newEntry entry 
+	if err := c.BindJSON(&newEntry); err != nil {
+		return
+	}
+
+}
+
+func getEntry(c *gin.Context) {
+	c.IndentedJSON(httpStatusOK, entries)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/entries", getEntry)
+	router.Run("localhost:8080")
+}
