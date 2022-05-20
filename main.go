cfdca7ea317313a05c4b2d025b946287ca4aeed9
Ahmed El-Gaaly <elgaalya@gmail.com>
2022-05-20 18:50:39 -0400

    fixed up some syntax errors

--- main.go
+++ main.go
@@ -2,33 +2,32 @@ package main
 
 import (
 	"net/http"
+
 	"github.com/gin-gonic/gin"
-	"errors"
 )
 
 type entry struct {
-	ID      string json: "id"
-	Entry   string json: "entry"
-	Date    string json: "date"
-	User_ID string json: "user_id"
+	ID      string `json:"id"`
+	Entry   string `json:"entry"`
+	Date    string `json:"date"`
+	User_ID string `json:"user_id"`
 }
 
-var entries = []webpost{
+var entries = []entry{
 	{ID: "1", Entry: "My first message", Date: "05/18/2022", User_ID: "user1"},
 	{ID: "2", Entry: "Hi name is user2", Date: "05/19/2022", User_ID: "user2"},
-	{ID: "3", Entry: "I made third message", Date: "05/20/2022", User_ID: "user1"}
+	{ID: "3", Entry: "I made third message", Date: "05/20/2022", User_ID: "user1"},
 }
 
-func createEntry(c *gin.Context) {
-	var newEntry entry 
-	if err := c.BindJSON(&newEntry); err != nil {
-		return
-	}
-
-}
+// func createEntry(c *gin.Context) {
+// 	var newEntry entry
+// 	if err := c.BindJSON(&newEntry); err != nil {
+// 		return
+// 	}
+// }
 
 func getEntry(c *gin.Context) {
-	c.IndentedJSON(httpStatusOK, entries)
+	c.IndentedJSON(http.StatusOK, entries)
 }
 
 func main() {
