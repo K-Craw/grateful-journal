8d223fa25b7cd597569c79b856e46851f7ff4352
Ahmed El-Gaaly <elgaalya@gmail.com>
2022-05-21 21:15:54 -0400

    worked on getEntry - still needs to be finished

--- getEntry.go
+++ getEntry.go
@@ -12,7 +12,21 @@ import (
 func GetEntry(c *gin.Context) {
 	db, err := sql.Open("sqlite3", "./entries.db")
 	checkErr(err)
-	db.Exec("SELECT json_group_array(json_object('id', id, 'entry', entry, 'date', date, 'user_name', user_name)) AS json_result FROM (SELECT * FROM entries ORDER BY id);")
-	db.Close()
-	c.IndentedJSON(http.StatusOK, entries)
+	stmt, err := db.Query("SELECT json_group_array(json_object('id', id, 'entry', entry, 'date', date, 'user_name', user_name)) AS json_result FROM (SELECT * FROM entries ORDER BY id);")
+	err = stmt.Err()
+	checkErr(err)
+	msg := make([]entry, 0)
+
+	for stmt.Next() {
+		theEntry := entry{}
+		err = stmt.Scan(&theEntry.ID, theEntry.Entry, theEntry.Date, theEntry.User_name)
+		checkErr(err)
+		msg = append(msg, theEntry)
+	}
+
+	err = stmt.Err()
+	checkErr(err)
+	// db.Exec("SELECT json_group_array(json_object('id', id, 'entry', entry, 'date', date, 'user_name', user_name)) AS json_result FROM (SELECT * FROM entries ORDER BY id);")
+	c.IndentedJSON(http.StatusOK, msg)
+	stmt.Close()
 }
--- main.go
+++ main.go
@@ -18,9 +18,10 @@ type entry struct {
 }
 
 var entries = []entry{
-	{ID: 1, Entry: "My first message", Date: "05/18/2022", User_name: "user1"},
-	{ID: 2, Entry: "Hi name is user2", Date: "05/19/2022", User_name: "user2"},
-	{ID: 3, Entry: "I made third message", Date: "05/20/2022", User_name: "user1"},
+	{ID: 1, Entry: "null", Date: "null", User_name: "null"},
+	// {ID: 1, Entry: "My first message", Date: "05/18/2022", User_name: "user1"},
+	// {ID: 2, Entry: "Hi name is user2", Date: "05/19/2022", User_name: "user2"},
+	// {ID: 3, Entry: "I made third message", Date: "05/20/2022", User_name: "user1"},
 }
 
 func checkErr(err error) {
